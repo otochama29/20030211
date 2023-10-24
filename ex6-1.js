91928fe262c360984efe66ce5837a9b206b3a4a7
otochama29 <131091873+otochama29@users.noreply.github.com>
2023-10-24 09:35:48 +0900

    Update ex6-1.js

--- ex6-1.js
+++ ex6-1.js
@@ -1,3 +1,13 @@
 <!--function sayhello61(){alert('Hello, Function!');}-->
 
 function saygoodbye61(){alert('Goodbye.');}
+
+var who65 ='world';
+function sayhello65(){
+  alert('Hello,'+who65+'!');
+}
+function taro65(){
+  who65='太郎';
+  function hanako65(){
+    who65='花子';
+  }
