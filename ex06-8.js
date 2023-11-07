d3975e7ba2935541ae370ae197fd9b3400adc34b
otochama29 <131091873+otochama29@users.noreply.github.com>
2023-11-07 09:37:33 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -4,4 +4,4 @@ function hanako68(){ document.getElementByld('who68').innerHTML='花子';}
 
 
  function gazo688()
- {document.getElementByld('who688').innerHTML='<img src="./image/テレサ.jpg" alt=オン">';}
+ {document.getElementByld('who688').innerHTML='<img src="./image/テレサ.jpg" alt="オン">';}
