abf04623e0c9adc5ec09cdd252abfd3d481128b0
otochama29 <131091873+otochama29@users.noreply.github.com>
2023-11-14 09:24:23 +0900

    Update ex06-8.js

--- ex06-8.js
+++ ex06-8.js
@@ -1,7 +1,4 @@
-function sayhello68(){alert('Hello, ' + document.getElementByld('who68').innerHTML+'!');}
-function taro68(){document.getElementByld('who68').innerHTML='太郎';}
-function hanako68(){document.getElementByld('who68').innerHTML='花子';}
+function sayhello68(){alert('Hello, ' +document.getElementByld('who68').innerHTML+'!');}
+function taro68(){document.getElementById('who68').innerHTML='太郎';}
+function hanako68(){document.getElementById('who68').innerHTML='花子';}
 
-
- function gazo688()
- {document.getElementByld('who688').innerHTML='<img src="./image/teresa.gif" alt="オン">';}
