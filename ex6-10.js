71ca9365989a3b1e5ce14c620533197cbf87d178
otochama29 <131091873+otochama29@users.noreply.github.com>
2023-11-14 10:36:37 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('logo').src='image/teresa.gif';}
-function imgchange2(){document.getElementById('logo').src='image/teresaa.gif';}
+function imgchange1(){document.getElementById('terechan').src='image/teresa.gif';}
+function imgchange2(){document.getElementById('terechan').src='image/teresaa.gif';}
