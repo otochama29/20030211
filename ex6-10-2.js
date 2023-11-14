a4cdb314a31ff49ee263b044324bcc4eac32bdc9
otochama29 <131091873+otochama29@users.noreply.github.com>
2023-11-14 10:23:45 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,2 +1,2 @@
-function imgchange3(){document.getElementById('logo').src ='./image/sai.jpg';}
-function imgchange4(){document.getElementById('logo').src ='./image/rogo.jpg';}
+function imgchange3(){document.getElementById('logo').src ='image/josai-logo.jpg';}
+function imgchange4(){document.getElementById('logo').src ='image/josai.logo.jpg';}
