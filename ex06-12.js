c2b3e0e7fb303bb6b62601de175b0c34a7343c86
otochama29 <131091873+otochama29@users.noreply.github.com>
2023-11-28 10:16:15 +0900

    Update ex06-12.js

--- ex06-12.js
+++ ex06-12.js
@@ -4,12 +4,12 @@ var str ="一文字ずつ表示します";
 //テキストボックスの文字数
 var cnt =document.timer.moji.value.length;
 //文字が全部表示されているかを確認
-if(cnt < 11){
-//現在よりも1文字多く切り出して表示
+if( cnt < 11 ){
+//現在より1文字多く切り出して表示
 document.timer.moji.value = str.substr(0,cnt+1);}
 else{
-//全て表示されたら、空文字に戻す
+//すべてに表示されたら、空文字に戻す
 document.timer.moji.value = "";}}
 function startfnc(){
 //関数hyoji()を1000ミリ秒間隔で呼び出す
-setInterval("hyoji()",1000); }
+setinterval("hyoji()",1000);}
