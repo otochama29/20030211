c029f615dbcde3ec37acac9427e0704addffdaad
otochama29 <131091873+otochama29@users.noreply.github.com>
2023-11-28 10:10:33 +0900

    Update ex06-12.js

--- ex06-12.js
+++ ex06-12.js
@@ -1,10 +1,9 @@
 function hyoji(){
 //表示する文字
-var str ="一文字ずつ表示します。";
+var str ="一文字ずつ表示します";
 //テキストボックスの文字数
-var cnt = document.timer.moji.value.length;
-//文字が全部表示されているか確認
-
+var cnt =document.timer.moji.value.length;
+//文字が全部表示されているかを確認
 if(cnt < 11){
 //現在よりも1文字多く切り出して表示
 document.timer.moji.value = str.substr(0,cnt+1);}
