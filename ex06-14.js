1e3c0fded79f493758b16334a30abe55aa158c11
otochama29 <131091873+otochama29@users.noreply.github.com>
2023-12-19 10:12:35 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,6 +1,6 @@
 //画像が変更する
-function hyoji14(){document.getElementById('logo1').src='./image/mutyuru.';}
-function hyoji15(){document.getElementById('logo1').src='./image/rujura.';}
+function hyoji14(){document.getElementById('logo1').src='./image/mutyuru2.';}
+function hyoji15(){document.getElementById('logo1').src='./image/mutyuru.';}
 
 function hyoji16(){document.getElementById('logo2').src='./image/mutyuru.jpg';}
 function hyoji17(){document.getElementById('logo2').src='./image/rujura.jpg';}
