d6fd27c26049c05593e9d8d31a48add7f2a02517
otochama29 <131091873+otochama29@users.noreply.github.com>
2024-01-13 06:31:06 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,5 +1,5 @@
 function page() {
-    document.getElementById('fuyu').src = './image/papanda.png';
+    document.getElementById('fuyu').src = './image/papanda.jpg';
 }
 function page2() {
     document.getElementById('fuyu').src = './image/panda.jpg';
