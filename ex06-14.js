365a9c025e45d8d7d76514d67f8be3c110ceb0cb
otochama29 <131091873+otochama29@users.noreply.github.com>
2024-01-12 19:41:49 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,4 +1,2 @@
-var img;
-function imgchange5(){ document.getElementById('panda').src='./image/panda.jpg'; setTimeout("imgchange6()",3000);}
-function imgchange6(){ document.getElementById('panda').src='./image/papanda.jpg'; }
-
+function page(){document.getElementById('panda').src ='./image/papanda.png';}
+function page2(){document.getElementById('panda').src ='./image/panda.png';}
