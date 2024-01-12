3c8571eb259031575d5859e736fd0ae227695e5a
otochama29 <131091873+otochama29@users.noreply.github.com>
2024-01-12 22:02:14 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,2 +1,2 @@
-function imagechange5(){document.getElementById('mutyuru').src ='./image/papanda.jpg';}
+function imagechange5(){document.getElementByid('mutyuru').src ='./image/papanda.jpg';}
 function imagechange6(){document.getElementById('mutyuru').src ='./image/panda.jpg';}
